9a5fbcd4e021f7bfebd389f387b9fc41fee5b329
FangFeiyue <18766131540@163.com>
2021-03-22 13:08:40 +0800

    测试数据库连接

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+function connect() {
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: '8889',
+    password: 'root',
+    database: 'e-book'
+  })
+}
+
+app.get('/book/list', (req, response) => {
+  const conn = connect()
+  conn.query('select * from book', (err, res) => {
+    if (err) {
+      response.json({
+        err_code: 1,
+        msg: '数据库连接失败'
+      })
+      console.log('err', err)
+    }else {
+      response.json({
+        err_code: 0,
+        data: res
+      })
+    }
+    conn.end()
+  })
+})
 
 app.get('/', (req, res) => {
   res.send(new Date().toDateString())
